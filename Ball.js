9d4c4e4836c52e0208c2077bcfd1689262badedd
Tariq Horan <N00202437@iadt.ie>
2023-07-14 20:42:05 +0100

    winner

--- Ball.js
+++ Ball.js
@@ -1,47 +1,59 @@
 class BallRGB {
-	constructor(x, y, vx, vy,radius, r, g, b) {
-		//properties
-		this.pos = createVector(x,y);
-		this.vx = vx;
-		this.vy = vy;
-		this.radius = radius;
-		this.r = r;
-		this.g = g;
-		this.b = b;
-		//this.rotation = 0;
-	
-}
+  constructor(x, y, vx, vy, radius, r, g, b) {
+    //properties
+    this.pos = createVector(x, y);
+    this.vx = vx;
+    this.vy = vy;
+    this.radius = radius;
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    //this.rotation = 0;
+  }
 
-//function to draw the ball
-render() {
-	
-		//translate(this.x, this.y);
-		//rotate(this.rotation);
-		fill(this.r, this.g, this.b);
-		ellipse(this.pos.x, this.pos.y,ballRadius*2, ballRadius*2);
-		
-	} 
-	
-accelerateBall(ax,ay){
-	this.vx = this.vx + ax;
-	this.vy = this.vy + ay;
-} 
+  //function to draw the ball
+  render() {
+    //translate(this.x, this.y);
+    //rotate(this.rotation);
+    fill(this.r, this.g, this.b);
+    ellipse(this.pos.x, this.pos.y, ballRadius * 2, ballRadius * 2);
+  }
 
-	//function to move the ball
-	step() {
-		if (this.pos.y < 40) {
-			winner();
-		}
-		if (this.pos.x > 0 && this.pos.x < 440 && this.pos.y < 170 && this.pos.y < 150) {
-			gameOver();
-		} else if (this.pos.x > 130 && this.pos.x < 500 && this.pos.y < 290 && this.pos.y > 250) {
-			gameOver();
-		} else if (this.pos.x > 0 && this.pos.x < 440 && this.pos.y < 440 && this.pos.y > 420) {
-			gameOver();
-		} else if (this.pos.x < 90 || this.pos.x > 480) {
-			gameOver();
-		}
-		this.pos.x += this.vx;
-		this.pos.y += this.vy;
-	}
+  accelerateBall(ax, ay) {
+    this.vx = this.vx + ax;
+    this.vy = this.vy + ay;
+  }
+
+  //function to move the ball
+  step() {
+    if (this.pos.y < 40) {
+      winner();
+    }
+    if (
+      this.pos.x > 0 &&
+      this.pos.x < 440 &&
+      this.pos.y < 170 &&
+      this.pos.y < 150
+    ) {
+      gameOver();
+    } else if (
+      this.pos.x > 130 &&
+      this.pos.x < 500 &&
+      this.pos.y < 290 &&
+      this.pos.y > 250
+    ) {
+      gameOver();
+    } else if (
+      this.pos.x > 0 &&
+      this.pos.x < 440 &&
+      this.pos.y < 440 &&
+      this.pos.y > 420
+    ) {
+      gameOver();
+    } else if (this.pos.x < 90 || this.pos.x > 480) {
+      gameOver();
+    }
+    this.pos.x += this.vx;
+    this.pos.y += this.vy;
+  }
 }
